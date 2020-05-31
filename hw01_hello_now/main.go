3978a4b3574098f24fb8cf0946538847f652965d
Evgenii Bobkov <evbob.dev@galaksion.com>
2020-05-31 13:13:07 +0300

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+const layout = "2006-01-02 03:04:05 -0700 MST"
+
 func main() {
-	// Place your code here
+	localTime := getLocalTime()
+	exactTime := getExactTime()
+
+	fmt.Printf("current time: %s\n", localTime)
+	fmt.Printf("exact time: %s\n", exactTime)
+}
+
+func getLocalTime() string {
+	return time.Now().Round(0).Format(layout)
+}
+
+func getExactTime() string {
+	exactTime, err := ntp.Time("1.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		log.Fatalf("error: %s", err.Error())
+	}
+
+	return exactTime.Format(layout)
 }
