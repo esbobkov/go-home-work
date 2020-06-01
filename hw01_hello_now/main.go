9b69c3c8302ad64fb15db947896c29d57a6057d0
Evgenii Bobkov <evbob.dev@galaksion.com>
2020-06-01 15:24:18 +0300

    move err handle

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -12,7 +12,10 @@ const layout = "2006-01-02 03:04:05 -0700 MST"
 
 func main() {
 	localTime := getLocalTime()
-	exactTime := getExactTime()
+	exactTime, err := getExactTime()
+	if err != nil {
+		log.Fatalf("error: %s", err.Error())
+	}
 
 	fmt.Printf("current time: %s\n", localTime)
 	fmt.Printf("exact time: %s\n", exactTime)
@@ -22,11 +25,11 @@ func getLocalTime() string {
 	return time.Now().Round(0).Format(layout)
 }
 
-func getExactTime() string {
+func getExactTime() (string, error) {
 	exactTime, err := ntp.Time("1.beevik-ntp.pool.ntp.org")
 	if err != nil {
-		log.Fatalf("error: %s", err.Error())
+		return "", err
 	}
 
-	return exactTime.Format(layout)
+	return exactTime.Format(layout), err
 }
