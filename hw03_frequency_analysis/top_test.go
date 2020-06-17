d6948a2eb7d23974d7dfb39fbdd56fc468ffb327
Evgenii Bobkov <evbob.dev@galaksion.com>
2020-06-17 18:40:41 +0300

    HW3 is completed

--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -1,13 +1,9 @@
 package hw03_frequency_analysis //nolint:golint
 
 import (
+	"github.com/stretchr/testify/require"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
-
-// Change to true if needed
-var taskWithAsteriskIsCompleted = false
 
 var text = `Как видите, он  спускается  по  лестнице  вслед  за  своим
 	другом   Кристофером   Робином,   головой   вниз,  пересчитывая
@@ -43,18 +39,28 @@ var text = `Как видите, он  спускается  по  лестни�
 	посидеть у огня и послушать какую-нибудь интересную сказку.
 		В этот вечер...`
 
+var shortText = `ты он  то 
+					он  то
+                        то`
+
 func TestTop10(t *testing.T) {
 	t.Run("no words in empty string", func(t *testing.T) {
-		assert.Len(t, Top10(""), 0)
+		require.Len(t, Top10(""), 0)
 	})
 
 	t.Run("positive test", func(t *testing.T) {
-		if taskWithAsteriskIsCompleted {
-			expected := []string{"он", "а", "и", "что", "ты", "не", "если", "то", "его", "кристофер", "робин", "в"}
-			assert.Subset(t, expected, Top10(text))
-		} else {
-			expected := []string{"он", "и", "а", "что", "ты", "не", "если", "-", "то", "Кристофер"}
-			assert.ElementsMatch(t, expected, Top10(text))
-		}
+		expected := []string{"он", "и", "а", "что", "ты", "не", "если", "-", "то", "Кристофер"}
+		require.ElementsMatch(t, expected, Top10(text))
+	})
+}
+
+func TestTopN(t *testing.T) {
+	t.Run("positive test", func(t *testing.T) {
+		expected := []string{"то", "он", "ты"}
+		require.Equal(t, expected, Top10(shortText))
+	})
+	t.Run("positive test", func(t *testing.T) {
+		expected := []string{"Donaudampfschiffahrtselektrizitätenhauptbetriebswerkbauunterbeamtengesellschaft"}
+		require.Equal(t, expected, Top10("Donaudampfschiffahrtselektrizitätenhauptbetriebswerkbauunterbeamtengesellschaft"))
 	})
 }
